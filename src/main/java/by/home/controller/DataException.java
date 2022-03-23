51b237430f59d8fd1a7e4b1f3aba75545fb47289
KonstantsinRudoy <78928176+IIpocToIIapeHb@users.noreply.github.com>
2022-03-23 08:28:34 +0300

    Delete src directory

--- src/main/java/by/home/controller/DataException.java
+++
@@ -1,9 +0,0 @@
-package by.home.controller;
-
-import java.io.IOException;
-
-public class DataException extends Exception {
-    public DataException(String message, IOException e) {
-        super(message, e);
-    }
-}
