50c71b0dbe6827fc8e21d46bebad24a9a1533b39
KonstantsinRudoy <78928176+IIpocToIIapeHb@users.noreply.github.com>
2022-04-01 17:57:29 +0300

    Delete src/main/java/by/tc/task01 directory

--- src/main/java/by/tc/task01/dao/creator/ApplianceCreator.java
+++
@@ -1,9 +0,0 @@
-package by.tc.task01.dao.creator;
-
-import by.tc.task01.entity.Appliance;
-import by.tc.task01.entity.criteria.Criteria;
-
-public interface ApplianceCreator {
-	Appliance create(Criteria criteria);
-
-}
--- src/main/java/by/tc/task01/dao/impl/DAOException.java
+++
@@ -1,9 +0,0 @@
-package by.tc.task01.dao.impl;
-
-public class DAOException extends Exception {
-
-	public DAOException(String message, Exception e) {
-		super(message, e);
-	}
-
-}
